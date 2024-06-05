a3fd4cbf318770430374d6652a6f6e17ab16cf22
Rafael Oliveira <yraffa.olive@gmail.com>
2024-06-05 20:49:15 -0300

    Update gs_python.py

--- gs_python.py
+++ gs_python.py
@@ -1,4 +1,3 @@
-
 # ----- FUNÇÕES ----- #
 
 def forcaNumerico(msg):
@@ -26,33 +25,25 @@ def buscaIndice(elemento, lista):
         if lista[i] == elemento:  
             return i
         
-def registrarResiduo(lista):
-    elemento = input('Digite o residuo que foi filtrado: ')
-    erro = ', '.join(lista)
-    if elemento in lista:
-        return elemento
-    else:
-        print(f'\n > Digite uma destas opcoes: {erro}. \n')  
-        
         
 # ----- PROGRAMA ----- #
 
-opcoes = ['REGISTRAR', 'VERIFICAR META', 'VISUALIZAR REGISTRADOS', 'SAIR']
+lista_opcoes = ['REGISTRAR', 'METAS', 'REGISTROS', 'SAIR']
 lista_residuos = ['Plasticos', 'Microplasticos', 'Poluentes Liquidos', 'Organicos']
 lista_quantidades = [0, 0, 0, 0]
-metas = [50, 100, 150, 50]
+lista_metas = [50, 100, 150, 50]
 
 while True:
-    print('\n > Sistema de gestão de residuos filtrados: \n')
+    print('\n > Sistema de gestão de residuos coletados e filtrados: \n')
     print('1. Registrar residuo filtrado \n'
           '2. Verificar meta dos residuos filtrados \n'
           '3. Visualizar residuos filtrados \n'
           '4. Sair')
 
-    erro = ', '.join(opcoes)
+    erro = ', '.join(lista_opcoes)
     print(f'\n > Digite uma destas opcoes: {erro}. \n')
 
-    escolha = forcaEscolha(opcoes, 'Escolha uma opcao: ')
+    escolha = forcaEscolha(lista_opcoes, 'Escolha uma opcao: ')
     
     if escolha == 'REGISTRAR':
         print('\n > RESIDUOS: \n')
@@ -64,24 +55,30 @@ while True:
         residuo = forcaEscolha(lista_residuos, 'Digite o residuo que foi filtrado: ')
         i = buscaIndice(residuo, lista_residuos)
         
-        qtd = forcaNumerico(f'Digite a quantidade de {lista_residuos[i]} que foi filtrada: ')
+        qtd = forcaNumerico(f'Digite a quantidade de {lista_residuos[i]} que foi filtrada (Kg ou L): ')
         lista_quantidades[i] += qtd
 
         print('\n > Residuo registrado com sucesso: \n' 
               f'Residuo: {lista_residuos[i]} \n'
               f'Quantidade: {lista_quantidades[i]} Kg ou L \n')
-    
-    elif escolha == 'VERIFICAR META':
+
+    elif escolha == 'METAS':
         print('\n > METAS: \n')
+        for i in range(len(lista_residuos)):
+            if lista_quantidades[i] >= lista_metas[i]:
+                print(f'A meta de {lista_residuos[i]} coletados e filtrados para reciclagem FOI ALCANCADA.')
+            else:
+                print(f'A meta de {lista_residuos[i]} coletados e filtrados para reciclagem ainda NÃO FOI ALCANCADA.')
+            print('')
         
-    elif escolha == 'VISUALIZAR REGISTRADOS':
+    elif escolha == 'REGISTROS':
         print('\n > RESIDUOS REGISTRADOS: \n')
-        
         for i in range(len(lista_residuos)):
             print(f'Residuo: {lista_residuos[i]} \n'
                   f'Quantidade: {lista_quantidades[i]} Kg ou L \n')
         
     elif escolha == 'SAIR':
+        print('\n SAINDO...')
         break
 
-print('\n > Obrigado por utilizar nosso programa!!! ')
+print('\n > Obrigado por utilizar nosso sistema!!! \n')
